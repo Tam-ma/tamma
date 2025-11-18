59a331502199796b7ebdb789660bb78b9896e790
Mahmoud Darwish <meywd@msn.com>
2025-11-18 17:01:50 -0500

    worker

--- apps/doc-review/app/worker.ts
+++ apps/doc-review/app/worker.ts
@@ -1,15 +1,15 @@
 /**
  * Cloudflare Worker entry point with Durable Objects
  */
-
-import { createPagesFunctionHandler } from '@react-router/cloudflare';
+import { createRequestListener } from "@react-router/cloudflare";
+// @ts-expect-error - virtual module provided by React Router compiler
+import * as build from "virtual:react-router/server-build";
 
 // Export the Durable Object for real-time events
 export { EventBroadcaster } from './lib/events/event-broadcaster';
 
-// Export default handler from React Router
-export default createPagesFunctionHandler({
-  // @ts-expect-error - build/server is generated at build time by React Router framework
-  build: () => import('./build/server'),
-  mode: process.env.NODE_ENV,
-});
+const requestListener = createRequestListener(build);
+
+export default {
+  fetch: requestListener,
+};
